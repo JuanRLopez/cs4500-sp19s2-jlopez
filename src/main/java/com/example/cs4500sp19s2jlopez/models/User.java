4551b110230e0a289d998011398348b871ba4ebe
Juan Lopez <lopez.ju@husky.neu.edu>
2019-01-21 13:37:58 -0500

    added User model

---
+++ src/main/java/com/example/cs4500sp19s2jlopez/models/User.java
@@ -0,0 +1,58 @@
+package com.example.cs4500sp19s2jlopez.models;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String username, String password, String firstName,
+                String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
